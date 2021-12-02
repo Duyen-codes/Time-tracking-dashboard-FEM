3a38c34b378a9784959e795aea47cbd27c59e993
Duyen Nguyen <hongduyen0705@gmail.com>
2021-12-02 17:42:05 +0200

    second commit

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        // Typical action to be performed when the document is ready:
-        //document.getElementById("demo").innerHTML = xhttp.responseText; 
 
-        var response = JSON.parse(xhttp.responseText);
-        var data = response.data;
+const buttons = document.querySelectorAll('.timeframe');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        activateClickedButton(button)
+        const clickedOption = button.dataset.option
+        renderCards(clickedOption)
+    })
+});
 
+let data = []
 
-        console.log(data);
-        for (let i = 0; i < data.length; i++) {
-            console.log(data[i]);
-        }
+const activateClickedButton = button => {
+    buttons.forEach(b => b.classList.remove('active'))
+    button.classList.add('active')
+}
 
-        const timeFrames = document.querySelectorAll('.timeframe');
-        timeFrames.forEach(timeFrame => {
-            timeFrame.addEventListener('click', (e) => {
-                console.log(e.target.innerHTML);
-                if (e.target.innerHTML === "Daily") {
+const loadData = async () => {
+    const response = await fetch('data.json')
+    const fetchedData = await response.json()
+    data = fetchedData
+    buttons[1].click()
+}
 
-                }
-            })
-        })
-
-
+const renderCards = option => {
+    if (option === 'daily') {
+        return 'Yesterday'
     }
-};
-xhttp.open("GET", "data.json", true);
-xhttp.send();
+}

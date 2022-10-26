388acd6e4e594177b0e476d42d0243ca97306f07
Duyen-codes <hongduyen0705@gmail.com>
2022-10-26 13:12:49 +0300

    show data by deffault

--- script.js
+++ script.js
@@ -1,59 +1,57 @@
-
-const buttons = document.querySelectorAll('.timeframe');
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        activateClickedButton(button)
-        const clickedOption = button.dataset.option
-        renderCards(clickedOption)
-    })
+const buttons = document.querySelectorAll(".timeframe");
+buttons[0].classList.add("active");
+let clickedOption = "daily";
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log("btn clicked");
+    activateClickedButton(button);
+    clickedOption = button.dataset.option;
+    console.log("clickedOption", clickedOption);
+    renderCards(clickedOption);
+  });
 });
 
-
-const activateClickedButton = button => {
-    buttons.forEach(b =>
-        b.classList.remove('active'))
-    button.classList.add('active')
-}
+const activateClickedButton = (button) => {
+  buttons.forEach((b) => b.classList.remove("active"));
+  button.classList.add("active");
+};
 
 const loadData = async () => {
-    const response = await fetch('./data.json')
-    const fetchedData = await response.json()
-    return fetchedData;
-    // buttons[1].click()
-}
+  const response = await fetch("./data.json");
+  const fetchedData = await response.json();
+  return fetchedData;
+};
 
+loadData();
 const clearActivities = () => {
-    const activities = document.querySelectorAll('.activity-tracker_activity');
-    activities.forEach(a => a.remove())
-}
+  const activities = document.querySelectorAll(".activity-tracker_activity");
+  activities.forEach((a) => a.remove());
+};
 
 const renderCards = async (option) => {
-    const data = await loadData()
-    clearActivities()
-    const main = document.querySelector('main');
-    const calcTimeframe = (option) => {
-
-        if (option === 'daily') {
-            return 'Yesterday'
-        } else if (option === 'weekly') {
-            return 'Last week'
-        } else if (option === 'monthly') {
-            return 'Last month'
-        }
+  const data = await loadData();
+  clearActivities();
+  const main = document.querySelector("main");
+  const calcTimeframe = (option) => {
+    if (option === "daily") {
+      return "Yesterday";
+    } else if (option === "weekly") {
+      return "Last week";
+    } else if (option === "monthly") {
+      return "Last month";
     }
+  };
 
-    data.forEach(activity => {
-        console.log(data)
-        const name = activity.title
-        const activityClass = name.toLowerCase().replace(' ', '-')
-        const timeFrameData = activity.timeframes[option]
-        const previousTimeframe = calcTimeframe(option)
-        console.log(previousTimeframe)
+  data.forEach((activity) => {
+    const name = activity.title;
+    const activityClass = name.toLowerCase().replace(" ", "-");
+    const timeFrameData = activity.timeframes[option];
+    const previousTimeframe = calcTimeframe(option);
 
-        const section = document.createElement('section');
-        section.classList.add('card', 'activity-tracker_activity', activityClass)
+    const section = document.createElement("section");
+    section.classList.add("card", "activity-tracker_activity", activityClass);
 
-        const stringToInject = `
+    const stringToInject = `
              <div class="card-banner"><img class="card-banner-icon" src="./images/icon-${activityClass}.svg" alt=""></div>
 
             <div class="card-body">
@@ -68,10 +66,11 @@ const renderCards = async (option) => {
                 </div>
 
             </div>
-        `
-        section.innerHTML = stringToInject;
-        console.log(section);
-        main.append(section);
-    });
+        `;
+    section.innerHTML = stringToInject;
 
-}
+    main.append(section);
+  });
+};
+
+window.onload = renderCards(clickedOption);
